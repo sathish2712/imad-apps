a09b29dd93e682db285021ec6d2ae9149b36f1af
sathish <sathish27121998@gmail.com>
2017-09-06 14:04:11 +0000

    tested!!

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log('helo worls');
+function som(){
+	console.log("this is samle text!!!");
+}
 
+function sample(){
+	console.log("this is some text");
+}
+console.log('helo worls');
+sample();
+som();
